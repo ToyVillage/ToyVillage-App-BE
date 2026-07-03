6ff18ea9932d0f35a5fe8c4880b57b76b259abe7
Kanghee <kh63167574@gmail.com>
2026-07-04 02:16:17 +0900

    feat : 공지 삭제 기능 추가 (엔티티 레포지토리)

---
+++ src/main/java/com/example/toyvillageappbe/domain/notice/domain/repository/NoticeRepository.java
@@ -0,0 +1,9 @@
+package com.example.toyvillageappbe.domain.notice.domain.repository;
+
+import com.example.toyvillageappbe.domain.notice.domain.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
